c6d68bd1ebf85e6bf9712d754cec36338e940030
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 21:10:51 +0100

    Add new java program

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+
+public class Person {
+
+	private String name;
+	
+		private int age;
+
+		public Person(String name) { 
+			this.name = name; 
+			}
+
+		public void setAge(int age) {
+		this.age = age;
+		}
+
+		public int getAge() { 
+			return age; 
+			}
+}
+
+
